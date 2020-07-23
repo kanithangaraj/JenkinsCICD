eae5c89a1f25d73191b59b5be99f367ae21e5c80
kanithangaraj <kanitha1996@yahoo.com>
2020-07-23 13:53:19 +0100

    ss

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("hello welcome to jenkins world ");
-        System.out.println("cicd");
+        System.out.println("cid");
     }
 }
